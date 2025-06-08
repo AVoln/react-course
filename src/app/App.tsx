ddf73e595a30e69977a4201519a26b70eb1f38a0
A.Voln <AVolnykh@IBS.RU>
2025-06-08 21:45:37 +0300

    половина 10 урока

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import "./styles/index.scss";
 import { Link } from "react-router-dom";
-import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "../pages/MainPage/MainPage.async";
+import { AboutPageAsync } from "../pages/AboutPage/AboutPage.async";
 import { Suspense } from "react";
-import { useTheme } from "./theme/useTheme";
-import { classNames } from "./helpers/classNames/classNames";
+import { useTheme } from "./providers/ThemeProvider/lib/useTheme";
+import { classNames } from "../helpers/classNames/classNames";
 
 export const App = () => {
   const { theme, toggleTheme } = useTheme();
