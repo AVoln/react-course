61f7b4b037249ccdfe33ae1feccd857808954400
A.Voln <a.voln@list.ru>
2025-07-06 15:39:57 +0300

    Финиш 1 раздела

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import "./styles/index.scss";
 import { Link } from "react-router-dom";
-import { MainPageAsync } from "../pages/MainPage/MainPage.async";
 import { Suspense } from "react";
 import { useTheme } from "../app/providers/ThemeProvider";
 import { classNames } from "../helpers/classNames/classNames";
-import { AboutPage } from "../pages/AboutPage";
+import { AboutPage } from "pages/AboutPage";
+import { MainPage } from "pages/MainPage";
 
 export const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -17,7 +17,7 @@ export const App = () => {
       <Link to={"/about"}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path={"/"} element={<MainPageAsync />} />
+          <Route path={"/"} element={<MainPage />} />
           <Route path={"/about"} element={<AboutPage />} />
         </Routes>
       </Suspense>
