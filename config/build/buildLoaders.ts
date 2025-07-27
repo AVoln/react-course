18558064a1fbeabdf8ac42c2c6708a5436d029a8
A.Voln <a.voln@list.ru>
2025-07-27 20:19:52 +0300

    17

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -8,6 +8,23 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     use: ["@svgr/webpack"],
   };
 
+  const babelLoader = {
+    test: /\.(js|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ["@babel/preset-env"],
+        plugins: [
+          [
+            "i18next-extract",
+            { locales: ["ru", "en"], keyAsDefaultValue: true },
+          ],
+        ],
+      },
+    },
+  };
+
   const cssLoder = {
     test: /\.s[ac]ss$/i,
     use: [
@@ -45,5 +62,5 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     ],
   };
 
-  return [fileLoader, svgLoader, typeScriptLoader, cssLoder];
+  return [fileLoader, svgLoader, babelLoader, typeScriptLoader, cssLoder];
 }
