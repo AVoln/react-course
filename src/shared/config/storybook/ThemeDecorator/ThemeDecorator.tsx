97f033062e3af28647c53602226fce262e6c472e
A.Voln <a.voln@list.ru>
2025-09-03 10:09:26 +0300

    storybook 80%

---
+++ src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -0,0 +1,13 @@
+import { StoryFn } from "@storybook/react";
+import { Theme } from "app/providers/ThemeProvider";
+import "app/styles/index.scss";
+
+export const ThemeDecorator = (theme: Theme) => {
+  return (StoryComponent: StoryFn) => {
+    return (
+      <div className={`app ${theme}`}>
+        <StoryComponent />
+      </div>
+    );
+  };
+};
---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import "app/styles/index.scss";
+import { ThemeDecorator } from "shared/config/storybook/ThemeDecorator/ThemeDecorator";
+import { Theme } from "app/providers/ThemeProvider";
+import { Sidebar } from "./Sidebar";
+
+const meta: Meta<typeof Sidebar> = {
+  title: "widget/Sidebar",
+  component: Sidebar,
+  tags: ["autodocs"],
+  argTypes: {},
+};
+
+export default meta;
+type Story = StoryObj<typeof Sidebar>;
+
+export const Light: Story = {
+  args: {},
+};
+
+export const Dark: Story = {
+  args: {},
+  decorators: [ThemeDecorator(Theme.DARK)],
+};
