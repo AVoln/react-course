cdc2b69011a624565d0928eca415d81a58d0e25b
A.Voln <a.voln@list.ru>
2025-09-28 13:15:21 +0300

    fix dev plugins

--- config/build/buildPlagins.ts
+++ config/build/buildPlagins.ts
@@ -8,7 +8,7 @@ export function buildPlagins({
   paths,
   isDev,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
+  const plugins = [
     new HtmlWebpackPlugin({
       template: paths.html,
     }),
@@ -20,9 +20,16 @@ export function buildPlagins({
     new DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
     }),
-    new webpack.HotModuleReplacementPlugin(),
-    new BundleAnalyzerPlugin({
-      openAnalyzer: false,
-    }),
   ];
+
+  if (isDev) {
+    plugins.push(
+      new BundleAnalyzerPlugin({
+        openAnalyzer: false,
+      })
+    );
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+  }
+
+  return plugins;
 }
